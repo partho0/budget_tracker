008a365ce013e0c6e5cb64814f4c9a090dbffa48
partho0 <68466005+partho0@users.noreply.github.com>
2022-06-07 21:55:06 +0600

    follow-up

--- js/BudgetTracker.js
+++ js/BudgetTracker.js
@@ -6,76 +6,75 @@ export default class BudgetTracker {
         this.root.querySelector(".new-entry").addEventListener("click", () => {
             this.onNewEntryBtnClick();
         });
-        //initial load
-        this.load
+
+        // Load initial data from Local Storage
+        this.load();
     }
 
     static html() {
         return `
-      <table class="budget-tracker">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Description</th>
-                    <th>Type</th>
-                    <th>Amount</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody class="entry">
-                
-            </tbody>
-            <tbody>
-                <tr>
-                    <td colspan="5" class="controls">
-                        <button type="button" class="new-entry">New Entry</button>
-                    </td>
-                </tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td colspan="5" class="summary">
-                        <strong>Total:</strong>
-                        <span class="total">$0.00</span>
-                    </td>
-                </tr>
-            </tfoot>
-        </table>
-      `;
+            <table class="budget-tracker">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Description</th>
+                        <th>Type</th>
+                        <th>Amount</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody class="entries"></tbody>
+                <tbody>
+                    <tr>
+                        <td colspan="5" class="controls">
+                            <button type="button" class="new-entry">New Entry</button>
+                        </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="5" class="summary">
+                            <strong>Total:</strong>
+                            <span class="total">$0.00</span>
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        `;
     }
 
     static entryHtml() {
         return `
-         <tr>
-                    <td>
-                        <input class="input input-date" type="date"></input>
-                    </td>
-                    <td>
-                        <input class="input input-description" type="text" placeholder="add a description"></input>
-                    </td>
-                    <td>
-                        <select class="input input-type">
-                           <option value="incomes">Income</option>
-                           <option value="expense">Expense</option>
-                        </select>
-                    </td>
-                    <td>
-                        <input type="number" class="input input-amount">
-                    </td>
-                    <td>
-                        <button type="button" class="delete-entry">&#10005;</button>
-                    </td>
-                </tr>
-      `;
+            <tr>
+                <td>
+                    <input class="input input-date" type="date">
+                </td>
+                <td>
+                    <input class="input input-description" type="text" placeholder="Add a Description (e.g. wages, bills, etc.)">
+                </td>
+                <td>
+                    <select class="input input-type">
+                        <option value="income">Income</option>
+                        <option value="expense">Expense</option>
+                    </select>
+                </td>
+                <td>
+                    <input type="number" class="input input-amount">
+                </td>
+                <td>
+                    <button type="button" class="delete-entry">&#10005;</button>
+                </td>
+            </tr>
+        `;
     }
 
     load() {
-        const entries = JSON.parse(localStorage.getItem("budget-tracker-dev") || "[]");
-
+        const entries = JSON.parse(localStorage.getItem("budget-tracker-entries-dev") || "[]");
 
         for (const entry of entries) {
             this.addEntry(entry);
         }
+
         this.updateSummary();
     }
 
@@ -84,13 +83,15 @@ export default class BudgetTracker {
             const amount = row.querySelector(".input-amount").value;
             const isExpense = row.querySelector(".input-type").value === "expense";
             const modifier = isExpense ? -1 : 1;
+
             return total + (amount * modifier);
         }, 0);
 
         const totalFormatted = new Intl.NumberFormat("en-US", {
             style: "currency",
-            currency: "BDT"
+            currency: "USD"
         }).format(total);
+
         this.root.querySelector(".total").textContent = totalFormatted;
     }
 
@@ -104,14 +105,14 @@ export default class BudgetTracker {
             };
         });
 
-        localStorage.setItem("budget-tracker-dev", JSON.stringify(data));
+        localStorage.setItem("budget-tracker-entries-dev", JSON.stringify(data));
         this.updateSummary();
     }
 
     addEntry(entry = {}) {
-        this.root.querySelector(".entry").insertAdjacentHTML("beforeend", BudgetTracker.entryHtml());
+        this.root.querySelector(".entries").insertAdjacentHTML("beforeend", BudgetTracker.entryHtml());
 
-        const row = this.root.querySelector(".entry tr:last-of-type");
+        const row = this.root.querySelector(".entries tr:last-of-type");
 
         row.querySelector(".input-date").value = entry.date || new Date().toISOString().replace(/T.*/, "");
         row.querySelector(".input-description").value = entry.description || "";
@@ -123,11 +124,11 @@ export default class BudgetTracker {
 
         row.querySelectorAll(".input").forEach(input => {
             input.addEventListener("change", () => this.save());
-        })
+        });
     }
 
     getEntryRows() {
-        return Array.from(this.root.querySelectorAll(".entry tr"));
+        return Array.from(this.root.querySelectorAll(".entries tr"));
     }
 
     onNewEntryBtnClick() {
